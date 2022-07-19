3bc3d8f4f97da569661ea7056231f3bd49bd0c2c
Nemanja Malesija <malesija13@gmail.com>
2022-07-19 22:41:55 +0200

    Add create player function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// selectors
+const fields = document.querySelector('.grid__element');
 
+// function create Player
+const player = (playerName, playerMark) => {
+  // get name, get mark
+  const getName = () => playerName;
+  const getMark = () => playerMark;
+  // set name, set mark
+  const setName = (value) => (value = playerName);
+  const setMark = (value) => (value = playerMark);
+
+  return {
+    getName,
+    getMark,
+    setName,
+    setMark,
+  };
+};
