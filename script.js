c3d6bc77b871b401665fca4aa2fecaf4b8b41f3e
Nemanja Malesija <malesija13@gmail.com>
2022-07-20 03:26:41 +0200

    Add factory function to store data

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // selectors
-const fields = document.querySelector('.grid__element');
+const cells = document.querySelectorAll('.grid__element');
 
 // function create Player
 const player = (playerName, playerMark) => {
@@ -17,3 +17,26 @@ const player = (playerName, playerMark) => {
     setMark,
   };
 };
+
+/// create game interface to store data
+const gameData = () => {
+  // board
+  const board = new Array(9).fill(null);
+
+  // get board value at a specific index
+  const getCell = (index) => board[index];
+
+  // set board value at a specific index
+  const setCell = (index, value) => (board[index] = value);
+
+  // get the entire board at any given time
+  const getBoard = () => [...board];
+
+  return {
+    getCell,
+    setCell,
+    getBoard,
+  };
+};
+
+const ticTacToe = gameData();
