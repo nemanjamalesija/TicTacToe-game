ab7e56ba584ec318803776fd845e777659e617ec
Nemanja Malesija <malesija13@gmail.com>
2022-07-20 04:27:23 +0200

    Add check winner function

--- script.js
+++ script.js
@@ -40,3 +40,76 @@ const gameData = () => {
 };
 
 const ticTacToe = gameData();
+
+// game logic
+
+const gameLogic = (function () {
+  // create players
+  const player1 = player('Player 1', 'X');
+  const player2 = player('Player 2', 'O');
+
+  let currentPlayer = player1;
+
+  // switch player
+  const switchPlayer = () => {
+    if (currentPlayer === player1) currentPlayer = player2;
+    else if (currentPlayer === player2) {
+      currentPlayer = player1;
+    }
+  };
+
+  // define win conditions
+  let winConditions = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+    [6, 4, 2],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
+  const checkWinner = (winConditions) => {
+    // get values at board[winCondition[element]]
+    function mapBoardValues(subArr) {
+      const elements = subArr.map((el) => {
+        return ticTacToe.getCell(el);
+      });
+
+      // test for equality
+      const solution = elements.every(
+        (el, i) => typeof el === 'string' && elements[i] === elements[0]
+      );
+
+      return solution;
+    }
+
+    // check if the condition is met in any sub array
+    const isWinner = winConditions.some((subArr) => mapBoardValues(subArr));
+
+    return isWinner;
+  };
+
+  // attach event listeners to each div
+
+  cells.forEach((cell, i) =>
+    cell.addEventListener('click', function (e) {
+      // on click push player.getMark on board[i]
+      if (ticTacToe.getCell(i) === null) {
+        ticTacToe.setCell(i, currentPlayer.getMark());
+
+        // set the value in the DOM
+        e.target.textContent = currentPlayer.getMark();
+
+        // switch player
+        switchPlayer();
+        console.log(ticTacToe.getBoard());
+
+        //check for winner
+        if (checkWinner(winConditions))
+          console.log(`${currentPlayer.getName()} won!`);
+      }
+    })
+  );
+})();
